e00e6d020507d1eb627d43d6b86cd270e197d06b
David Jensen <david@greenzie.com>
2021-03-16 11:31:17 -0400

    Modified node to re-connect after resetting the sensor

--- src/main.cpp
+++ src/main.cpp
@@ -159,122 +159,127 @@ int main(int argc, char *argv[])
 
     ROS_INFO("Connecting to : %s @ %d Baud", SensorPort.c_str(), SensorBaudrate);
 
-    // Create a VnSensor object and connect to sensor
-    VnSensor vs;
-
-    // Default baudrate variable
-    int defaultBaudrate;
-    // Run through all of the acceptable baud rates until we are connected
-    // Looping in case someone has changed the default
-    bool baudSet = false;
-    while(!baudSet){
-        // Make this variable only accessible in the while loop
-        static int i = 0;
-        defaultBaudrate = vs.supportedBaudrates()[i];
-        ROS_INFO("Connecting with default at %d", defaultBaudrate);
-        // Default response was too low and retransmit time was too long by default.
-        // They would cause errors
-        vs.setResponseTimeoutMs(1000); // Wait for up to 1000 ms for response
-        vs.setRetransmitDelayMs(50);  // Retransmit every 50 ms
-
-        // Acceptable baud rates 9600, 19200, 38400, 57600, 128000, 115200, 230400, 460800, 921600
-        // Data sheet says 128000 is a valid baud rate. It doesn't work with the VN100 so it is excluded.
-        // All other values seem to work fine.
-        try{
-            // Connect to sensor at it's default rate
-            if(defaultBaudrate != 128000 && SensorBaudrate != 128000)
-            {
-                vs.connect(SensorPort, defaultBaudrate);
-                // Issues a change baudrate to the VectorNav sensor and then
-                // reconnects the attached serial port at the new baudrate.
-                vs.changeBaudRate(SensorBaudrate);
-                // Only makes it here once we have the default correct
-                ROS_INFO("Connected baud rate is %d",vs.baudrate());
-                baudSet = true;
-            }
-        }
-        // Catch all oddities
-        catch(...){
-            // Disconnect if we had the wrong default and we were connected
-            vs.disconnect();
-            ros::Duration(0.2).sleep();
-        }
-        // Increment the default iterator
-        i++;
-        // There are only 9 available data rates, if no connection
-        // made yet possibly a hardware malfunction?
-        if(i > 8)
-        {
-            break;
-        }
-    }
-
-    // Now we verify connection (Should be good if we made it this far)
-    if(vs.verifySensorConnectivity())
-    {
-        ROS_INFO("Device connection established");
-    }else{
-        ROS_ERROR("No device communication");
-        ROS_WARN("Please input a valid baud rate. Valid are:");
-        ROS_WARN("9600, 19200, 38400, 57600, 115200, 128000, 230400, 460800, 921600");
-        ROS_WARN("With the test IMU 128000 did not work, all others worked fine.");
-    }
-    // Query the sensor's model number.
-    string mn = vs.readModelNumber();
-    ROS_INFO("Model Number: %s", mn.c_str());
-
-    // Set the device info for passing to the packet callback function
-    UserData user_data;
-    user_data.device_family = vs.determineDeviceFamily();
-
-    // Set Data output Freq [Hz]
-    vs.writeAsyncDataOutputFrequency(async_output_rate);
-
-    // Configure binary output message
-    BinaryOutputRegister bor(
-            ASYNCMODE_PORT1,
-            SensorImuRate / async_output_rate,  // update rate [ms]
-            COMMONGROUP_QUATERNION
-            | COMMONGROUP_ANGULARRATE
-            | COMMONGROUP_POSITION
-            | COMMONGROUP_ACCEL
-            | COMMONGROUP_MAGPRES,
-            TIMEGROUP_NONE,
-            IMUGROUP_NONE,
-            GPSGROUP_NONE,
-            ATTITUDEGROUP_YPRU, //<-- returning yaw pitch roll uncertainties
-            INSGROUP_INSSTATUS
-            | INSGROUP_POSLLA
-            | INSGROUP_POSECEF
-            | INSGROUP_VELBODY
-            | INSGROUP_ACCELECEF);
-
-    vs.writeBinaryOutput1(bor);
-
-    // Set Data output Freq [Hz]
-    vs.writeAsyncDataOutputFrequency(async_output_rate);
-    vs.registerAsyncPacketReceivedHandler(&user_data, BinaryAsyncMessageReceived);
-
-    // You spin me right round, baby
-    // Right round like a record, baby
-    // Right round round round
-    ros::Rate loop_rate(10);
     while (ros::ok())
     {
-        if (need_to_reset)
-        {
-            bool wait_for_reply = true;
-            vs.reset(wait_for_reply);
-            need_to_reset = false;
+        // Create a VnSensor object and connect to sensor
+        VnSensor vs;
+
+        // Default baudrate variable
+        int defaultBaudrate;
+        // Run through all of the acceptable baud rates until we are connected
+        // Looping in case someone has changed the default
+        bool baudSet = false;
+        while(!baudSet){
+            // Make this variable only accessible in the while loop
+            static int i = 0;
+            defaultBaudrate = vs.supportedBaudrates()[i];
+            ROS_INFO("Connecting with default at %d", defaultBaudrate);
+            // Default response was too low and retransmit time was too long by default.
+            // They would cause errors
+            vs.setResponseTimeoutMs(1000); // Wait for up to 1000 ms for response
+            vs.setRetransmitDelayMs(50);  // Retransmit every 50 ms
+
+            // Acceptable baud rates 9600, 19200, 38400, 57600, 128000, 115200, 230400, 460800, 921600
+            // Data sheet says 128000 is a valid baud rate. It doesn't work with the VN100 so it is excluded.
+            // All other values seem to work fine.
+            try{
+                // Connect to sensor at it's default rate
+                if(defaultBaudrate != 128000 && SensorBaudrate != 128000)
+                {
+                    vs.connect(SensorPort, defaultBaudrate);
+                    // Issues a change baudrate to the VectorNav sensor and then
+                    // reconnects the attached serial port at the new baudrate.
+                    vs.changeBaudRate(SensorBaudrate);
+                    // Only makes it here once we have the default correct
+                    ROS_INFO("Connected baud rate is %d",vs.baudrate());
+                    baudSet = true;
+                }
+            }
+            // Catch all oddities
+            catch(...){
+                // Disconnect if we had the wrong default and we were connected
+                vs.disconnect();
+                ros::Duration(0.2).sleep();
+            }
+            // Increment the default iterator
+            i++;
+            // There are only 9 available data rates, if no connection
+            // made yet possibly a hardware malfunction?
+            if(i > 8)
+            {
+                break;
+            }
         }
-        ros::spinOnce(); // Need to make sure we disconnect properly. Check if all ok.
+
+        // Now we verify connection (Should be good if we made it this far)
+        if(vs.verifySensorConnectivity())
+        {
+            ROS_INFO("Device connection established");
+        }else{
+            ROS_ERROR("No device communication");
+            ROS_WARN("Please input a valid baud rate. Valid are:");
+            ROS_WARN("9600, 19200, 38400, 57600, 115200, 128000, 230400, 460800, 921600");
+            ROS_WARN("With the test IMU 128000 did not work, all others worked fine.");
+        }
+        // Query the sensor's model number.
+        string mn = vs.readModelNumber();
+        ROS_INFO("Model Number: %s", mn.c_str());
+
+        // Set the device info for passing to the packet callback function
+        UserData user_data;
+        user_data.device_family = vs.determineDeviceFamily();
+
+        // Set Data output Freq [Hz]
+        vs.writeAsyncDataOutputFrequency(async_output_rate);
+
+        // Configure binary output message
+        BinaryOutputRegister bor(
+                ASYNCMODE_PORT1,
+                SensorImuRate / async_output_rate,  // update rate [ms]
+                COMMONGROUP_QUATERNION
+                | COMMONGROUP_ANGULARRATE
+                | COMMONGROUP_POSITION
+                | COMMONGROUP_ACCEL
+                | COMMONGROUP_MAGPRES,
+                TIMEGROUP_NONE,
+                IMUGROUP_NONE,
+                GPSGROUP_NONE,
+                ATTITUDEGROUP_YPRU, //<-- returning yaw pitch roll uncertainties
+                INSGROUP_INSSTATUS
+                | INSGROUP_POSLLA
+                | INSGROUP_POSECEF
+                | INSGROUP_VELBODY
+                | INSGROUP_ACCELECEF);
+
+        vs.writeBinaryOutput1(bor);
+
+        // Set Data output Freq [Hz]
+        vs.writeAsyncDataOutputFrequency(async_output_rate);
+        vs.registerAsyncPacketReceivedHandler(&user_data, BinaryAsyncMessageReceived);
+
+        // You spin me right round, baby
+        // Right round like a record, baby
+        // Right round round round
+        ros::Rate loop_rate(10);
+        while (ros::ok())
+        {
+            if (need_to_reset)
+            {
+                bool wait_for_reply = true;
+                vs.reset(wait_for_reply);
+                need_to_reset = false;
+                break;
+            }
+            ros::spinOnce(); // Need to make sure we disconnect properly. Check if all ok.
+        }
+
+        // Node has been terminated
+        vs.unregisterAsyncPacketReceivedHandler();
+        ros::Duration(0.5).sleep();
+        vs.disconnect();
+        ros::Duration(0.5).sleep();
     }
 
-    // Node has been terminated
-    vs.unregisterAsyncPacketReceivedHandler();
-    ros::Duration(0.5).sleep();
-    vs.disconnect();
-    ros::Duration(0.5).sleep();
     return 0;
 }
 
